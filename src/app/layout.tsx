fc09175911996c912e60a60b1179994ee82dcbf2
RenanSui <renanddtao@gmail.com>
2024-04-30 10:46:25 -0300

    suppress html hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={cn(
           'bg-[url(/images/img-noise-361x370.png)] bg-white min-h-screen bg-background font-sans antialiased',
