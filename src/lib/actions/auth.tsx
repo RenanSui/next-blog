a38f2314c47ca22493efa73eba1ada0963b8f9ae
RenanSui <renanddtao@gmail.com>
2024-04-30 10:18:34 -0300

    modify auth actions

--- src/lib/actions/auth.tsx
+++ src/lib/actions/auth.tsx
@@ -7,39 +7,41 @@ import { authSchema } from '../validations/auth'
 
 type Inputs = z.infer<typeof authSchema>
 
-export const signIn = async (formData: Inputs) => {
-  const parsed = authSchema.safeParse(formData)
+export const auth = {
+  signIn: async (formData: Inputs) => {
+    const parsed = authSchema.safeParse(formData)
 
-  if (!parsed.success) {
-    return {
-      error: {
-        message: parsed.error.errors.map((error) => error.message).join('. '),
-      },
+    if (!parsed.success) {
+      return {
+        error: {
+          message: parsed.error.errors.map((error) => error.message).join('. '),
+        },
+      }
     }
-  }
 
-  const cookieStore = cookies()
+    const cookieStore = cookies()
 
-  const accessToken = cookieStore.get('accessToken')?.value ?? ''
-  const response = await fetch(`${process.env.SERVER_URL}/auth/sign-in`, {
-    method: 'POST',
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      authorization: `Bearer ${accessToken}`,
-    },
-    body: JSON.stringify({ ...parsed.data }),
-  })
-
-  const { data, message, status }: HTTPResponse<Auth> = await response.json()
-
-  if (data?.accessToken) {
-    cookieStore.set('accessToken', data.accessToken, {
-      httpOnly: true,
-      sameSite: 'lax',
-      maxAge: 1000 * 60 * 60 * 24,
+    const accessToken = cookieStore.get('accessToken')?.value ?? ''
+    const response = await fetch(`${process.env.SERVER_URL}/auth/sign-in`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+        authorization: `Bearer ${accessToken}`,
+      },
+      body: JSON.stringify({ ...parsed.data }),
     })
-  }
 
-  return { message, status }
+    const { data, message, status }: HTTPResponse<Auth> = await response.json()
+
+    if (data?.accessToken) {
+      cookieStore.set('accessToken', data.accessToken, {
+        httpOnly: true,
+        sameSite: 'lax',
+        maxAge: 1000 * 60 * 60 * 24,
+      })
+    }
+
+    return { message, status }
+  },
 }
