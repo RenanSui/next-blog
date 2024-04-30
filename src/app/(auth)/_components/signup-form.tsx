3e190074a4a48c854a3c8e8adbb32c2f6ce4eb7c
RenanSui <renanddtao@gmail.com>
2024-04-30 11:48:05 -0300

    create signup-form

---
+++ src/app/(auth)/_components/signup-form.tsx
@@ -0,0 +1,109 @@
+'use client'
+
+import { Icons } from '@/components/icons'
+import { PasswordInput } from '@/components/password-input'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { signUp } from '@/lib/actions/auth'
+import { AuthErrorHandler } from '@/lib/handle-auth-error'
+import { authSchema } from '@/lib/validations/auth'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useRouter } from 'next/navigation'
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import type { z } from 'zod'
+
+type Inputs = z.infer<typeof authSchema>
+
+export default function SignUpForm() {
+  const [loading, setLoading] = React.useState(false)
+  const router = useRouter()
+
+  // react-hook-form
+  const form = useForm<Inputs>({
+    resolver: zodResolver(authSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  })
+
+  async function onSubmit(data: Inputs) {
+    setLoading(true)
+    try {
+      const { error, status } = await signUp({ ...data })
+
+      if (error && error?.message) {
+        throw new Error(error?.message)
+      }
+
+      AuthErrorHandler(status)
+
+      router.push(`${window.location.origin}/`)
+    } catch (err) {
+      toast.error(err as string)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form className="grid gap-4" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="rodneymullen180@gmail.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage className="dark:text-red-500" />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="**********" {...field} />
+              </FormControl>
+              <FormMessage className="dark:text-red-500" />
+            </FormItem>
+          )}
+        />
+        <Button
+          type="submit"
+          className="mt-2 bg-neutral-900 text-neutral-50 shadow hover:bg-neutral-900/90 dark:bg-neutral-900 dark:text-neutral-50 dark:hover:bg-neutral-900/90"
+          disabled={loading}
+        >
+          {loading && (
+            <Icons.spinner
+              className="mr-2 size-4 animate-spin text-white"
+              aria-hidden="true"
+            />
+          )}
+          Sign up
+          <span className="sr-only">Sign up</span>
+        </Button>
+      </form>
+    </Form>
+  )
+}
