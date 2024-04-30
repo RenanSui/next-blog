61a7abfe4d0b5f28125de6694aa84ca9de15def4
RenanSui <renanddtao@gmail.com>
2024-04-30 11:47:55 -0300

    extend auth action with signup

--- src/lib/actions/auth.ts
+++ src/lib/actions/auth.ts
@@ -43,3 +43,40 @@ export const signIn = async (formData: Inputs) => {
 
   return { message, status }
 }
+
+export const signUp = async (formData: Inputs) => {
+  const parsed = authSchema.safeParse(formData)
+
+  if (!parsed.success) {
+    return {
+      error: {
+        message: parsed.error.errors.map((error) => error.message).join('. '),
+      },
+    }
+  }
+
+  const cookieStore = cookies()
+
+  const accessToken = cookieStore.get('accessToken')?.value ?? ''
+  const response = await fetch(`${process.env.SERVER_URL}/auth/sign-up`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ ...parsed.data }),
+  })
+
+  const { data, message, status }: HTTPResponse<Auth> = await response.json()
+
+  if (data?.accessToken) {
+    cookieStore.set('accessToken', data.accessToken, {
+      httpOnly: true,
+      sameSite: 'lax',
+      maxAge: 1000 * 60 * 60 * 24,
+    })
+  }
+
+  return { message, status }
+}
