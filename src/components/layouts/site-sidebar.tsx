fad9494487daf2514b40348fca4be960f7474bd8
RenanSui <renanddtao@gmail.com>
2024-05-13 19:33:49 -0300

    modify site sidebar padding

--- src/components/layouts/site-sidebar.tsx
+++ src/components/layouts/site-sidebar.tsx
@@ -22,7 +22,7 @@ export default function SiteSidebar({
 
   return (
     <aside className={cn('h-screen w-full', className)} {...props}>
-      <div className="hidden h-[3.55rem] items-center border-border/60 px-6 lg:flex">
+      <div className="hidden h-[3.55rem] items-center border-border/60 lg:flex">
         <Link
           href="/"
           className="flex w-fit items-center font-heading tracking-wider text-foreground/90 transition-colors hover:text-foreground"
@@ -36,10 +36,8 @@ export default function SiteSidebar({
           )}
         </Link>
       </div>
-      <div className="flex flex-col gap-2.5 px-4 pt-2 lg:px-6 lg:pt-4">
-        {children}
-      </div>
-      <ScrollArea className="h-[calc(100vh-8rem)] px-3 py-2.5 lg:px-5">
+      <div className="flex flex-col gap-2.5 pt-2 lg:pt-4">{children}</div>
+      <ScrollArea className="h-[calc(100vh-8rem)] py-2.5">
         <SidebarNav items={siteConfig.mainNav} className="p-1 pt-4" />
       </ScrollArea>
     </aside>
