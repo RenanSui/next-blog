bfe568ddbdf1cc757b9f45bd675943485c18f180
RenanSui <renanddtao@gmail.com>
2024-05-13 17:52:46 -0300

    update lobby layout style

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -1,5 +1,7 @@
-import { SiteFooter } from '@/components/layouts/site-footer'
-import { SiteHeader } from '@/components/layouts/site-header'
+import { LobbyHeader } from '@/components/layouts/lobby-header'
+import LobbySidebar from '@/components/layouts/lobby-sidebar'
+import { LobbySidebarSheet } from '@/components/layouts/lobby-sidebar-sheet'
+import { SidebarProvider } from '@/hooks/use-sidebar'
 import { getUser } from '@/lib/actions/user'
 
 export default async function LobbyLayout({
@@ -10,10 +12,21 @@ export default async function LobbyLayout({
   const user = await getUser()
 
   return (
-    <div className="relative flex min-h-screen flex-col">
-      <SiteHeader user={user} />
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
+    <SidebarProvider>
+      <div className="grid min-h-screen w-full container lg:grid-cols-[17.5rem_1fr]">
+        <LobbySidebar
+          className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block"
+          user={user}
+        />
+        <div className="flex flex-col">
+          <LobbyHeader user={user}>
+            <LobbySidebarSheet user={user}>
+              <LobbySidebar user={user} />
+            </LobbySidebarSheet>
+          </LobbyHeader>
+          <main className="flex-1 overflow-hidden px-6">{children}</main>
+        </div>
+      </div>
+    </SidebarProvider>
   )
 }
