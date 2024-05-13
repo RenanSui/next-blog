3f1acbc70b9d069949acc0c04499e33145a94640
RenanSui <renanddtao@gmail.com>
2024-05-13 19:34:20 -0300

    extend lobby layout

--- src/app/(lobby)/layout.tsx
+++ src/app/(lobby)/layout.tsx
@@ -3,6 +3,7 @@ import SiteSidebar from '@/components/layouts/site-sidebar'
 import { SiteSidebarSheet } from '@/components/layouts/site-sidebar-sheet'
 import { SidebarProvider } from '@/hooks/use-sidebar'
 import { getUser } from '@/lib/actions/user'
+import LobbySidebar from './_components/lobby-sidebar'
 
 export default async function LobbyLayout({
   children,
@@ -13,18 +14,21 @@ export default async function LobbyLayout({
 
   return (
     <SidebarProvider>
-      <div className="grid min-h-screen w-full container lg:grid-cols-[17.5rem_1fr]">
+      <div className="grid min-h-screen w-full container lg:grid-cols-[17.5rem_1fr] ">
         <SiteSidebar
           className="top-0 z-30 hidden flex-col gap-4 border-r border-border/60 lg:sticky lg:block"
           user={user}
         />
-        <div className="flex flex-col">
-          <SiteHeader user={user}>
-            <SiteSidebarSheet user={user}>
-              <SiteSidebar user={user} />
-            </SiteSidebarSheet>
-          </SiteHeader>
-          <main className="flex-1 overflow-hidden px-6">{children}</main>
+        <div className="flex">
+          <div className="flex flex-col">
+            <SiteHeader user={user}>
+              <SiteSidebarSheet user={user}>
+                <SiteSidebar user={user} />
+              </SiteSidebarSheet>
+            </SiteHeader>
+            <main className="flex-1 overflow-hidden px-6">{children}</main>
+          </div>
+          <LobbySidebar className="top-0 max-w-[320px] p-4 z-30 hidden flex-col gap-4 border-l border-border/60 lg:sticky lg:block" />
         </div>
       </div>
     </SidebarProvider>
