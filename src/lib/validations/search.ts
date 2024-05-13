dd610f40c86aa79fdc74b0578ca762d4c88fa83e
RenanSui <renanddtao@gmail.com>
2024-05-13 19:32:51 -0300

    create search schema

---
+++ src/lib/validations/search.ts
@@ -0,0 +1,8 @@
+import { z } from 'zod'
+import * as zfd from 'zod-form-data'
+
+export const searchSchema = zfd.formData({
+  searchInput: zfd.text(z.string().min(3).max(50)),
+})
+
+export type SearchSchema = z.infer<typeof searchSchema>
