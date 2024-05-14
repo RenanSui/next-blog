a85b80e917484bc5ceecb7f602b79b18fdf59a70
RenanSui <renanddtao@gmail.com>
2024-05-13 21:13:24 -0300

    create theme-toggle

---
+++ src/components/layouts/theme-toggle.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import { Button } from '@/components/ui/button'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+import { LaptopIcon, MoonIcon, SunIcon } from '@radix-ui/react-icons'
+import { useTheme } from 'next-themes'
+import { useEffect } from 'react'
+
+export const ThemeToggle = ({ labelled }: { labelled?: boolean }) => {
+  const { setTheme, theme } = useTheme()
+
+  useEffect(() => {
+    setTimeout(() => theme)
+  }, [theme])
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          size={labelled ? 'default' : 'icon'}
+          aria-label="theme-toggler"
+        >
+          {labelled ? (
+            <span className="capitalize flex gap-2 item-center">
+              <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+              {theme}
+            </span>
+          ) : (
+            <>
+              <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </>
+          )}
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuItem role="light-toggle" onClick={() => setTheme('light')}>
+          <SunIcon className="mr-2 h-4 w-4" />
+          <span>Light</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem role="dark-toggle" onClick={() => setTheme('dark')}>
+          <MoonIcon className="mr-2 h-4 w-4" />
+          <span>Dark</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem
+          role="system-toggle"
+          onClick={() => setTheme('system')}
+        >
+          <LaptopIcon className="mr-2 h-4 w-4" />
+          <span>System</span>
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
