4bf4a2444861832fdb42ea9c60076d8f655b6b52
RenanSui <renanddtao@gmail.com>
2024-05-14 19:52:11 -0300

    create user avatar

---
+++ src/components/user-avatar.tsx
@@ -0,0 +1,19 @@
+import { getUser } from '@/lib/actions/user'
+import { cn } from '@/lib/utils'
+import * as React from 'react'
+import { Avatar, AvatarFallback } from './ui/avatar'
+
+type UserAvatarProps = React.HTMLAttributes<HTMLSpanElement> & {
+  user?: Awaited<ReturnType<typeof getUser>>
+}
+
+export default function UserAvatar({ className, user }: UserAvatarProps) {
+  const initials = `${user?.email.charAt(0)}`
+
+  return user ? (
+    <Avatar className={cn('size-8', className)}>
+      {/* <AvatarImage src={user.imageUrl} alt={user.username ?? ''} /> */}
+      <AvatarFallback className="capitalize">{initials}</AvatarFallback>
+    </Avatar>
+  ) : null
+}
