5be135736fdeb948b8f156482a288213ab1f0487
RenanSui <renanddtao@gmail.com>
2024-05-16 13:16:24 -0300

    add auth dropdown to site sitebar

--- src/components/layouts/site-sidebar.tsx
+++ src/components/layouts/site-sidebar.tsx
@@ -5,8 +5,8 @@ import Link from 'next/link'
 import * as React from 'react'
 import { Icons } from '../icons'
 import { ScrollArea } from '../ui/scroll-area'
+import { AuthDropdown } from './auth-dropdown'
 import { SidebarNav } from './sidebar-nav'
-import UserAvatar from '../user-avatar'
 
 type SiteSidebarProps = React.HTMLAttributes<HTMLElement> & {
   user: User | null
@@ -23,19 +23,18 @@ export default function SiteSidebar({
       <div className="hidden h-[3.55rem] items-center border-border/60 lg:flex">
         <Link
           href="/"
-          className="flex w-fit items-center font-heading tracking-wider text-foreground/90 transition-colors hover:text-foreground"
+          className="flex w-fit items-center font-heading tracking-wider hover:bg-muted transition-colors px-2 py-2 rounded-full"
         >
-          {user ? (
-            <UserAvatar user={user} />
-          ) : (
-            <Icons.menu aria-hidden="true" className="size-8" />
-          )}
+          <Icons.menu aria-hidden="true" className="size-9" />
         </Link>
       </div>
       <div className="flex flex-col gap-2.5 pt-2 lg:pt-4">{children}</div>
-      <ScrollArea className="h-[calc(100vh-8rem)] py-2.5">
-        <SidebarNav items={siteConfig.mainNav} className="p-1 pt-4" />
+      <ScrollArea className="h-[calc(100vh-8.5rem)] py-2.5">
+        <SidebarNav items={siteConfig.mainNav} className="pt-4" />
       </ScrollArea>
+      <div className="px-2">
+        <AuthDropdown user={user} />
+      </div>
     </aside>
   )
 }
