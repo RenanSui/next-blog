9dcfea607d15d727b0878e4fee1a2dd4de17a749
RenanSui <renanddtao@gmail.com>
2024-05-16 16:47:49 -0300

    create handle user error

---
+++ src/lib/handle-user-error.ts
@@ -0,0 +1,19 @@
+import { StatusCodes } from 'http-status-codes'
+
+export const HTPPUserErrorMessages = {
+  200: 'The request has succeeded.',
+  400: 'The server could not understand the request due to invalid syntax.',
+  401: 'the user must authenticate to use the services.',
+  403: 'The user does not have access rights to the content.',
+}
+
+export type UserStatusCode = keyof typeof HTPPUserErrorMessages
+
+export function UserErrorHandler(status?: UserStatusCode) {
+  if (!status) return
+
+  if (status === StatusCodes.OK) return
+
+  const message = HTPPUserErrorMessages[status]
+  throw new Error(message)
+}
