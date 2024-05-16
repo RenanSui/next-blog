3478b53b82c6490412217408257925e885819e4b
RenanSui <renanddtao@gmail.com>
2024-05-16 16:49:36 -0300

    extend user actions

--- src/lib/actions/user.ts
+++ src/lib/actions/user.ts
@@ -1,5 +1,10 @@
+'use server'
+
 import { HTTPResponse, User } from '@/types'
+import { revalidatePath } from 'next/cache'
 import { cookies } from 'next/headers'
+import { UserStatusCode } from '../handle-user-error'
+import { UpdateUserSchema } from '../validations/user'
 
 export const getUser = async () => {
   try {
@@ -15,11 +20,34 @@ export const getUser = async () => {
       },
     })
 
-    const { data: user }: HTTPResponse<User> = await response.json()
+    const { data: user }: HTTPResponse<User, UserStatusCode> =
+      await response.json()
 
     return user
   } catch (error) {
     const user = null
     return user
   }
+}
+
+export const updateProfile = async (formData: UpdateUserSchema) => {
+  const cookieStore = cookies()
+  const accessToken = cookieStore.get('accessToken')?.value ?? ''
+
+  const response = await fetch(`${process.env.SERVER_URL}/user/update`, {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      'Content-Type': 'application/json',
+      authorization: `Bearer ${accessToken}`,
+    },
+    body: JSON.stringify({ ...formData }),
+  })
+
+  const { status, message }: HTTPResponse<User, UserStatusCode> =
+    await response.json()
+
+  revalidatePath('/')
+
+  return { message, status }
 }
