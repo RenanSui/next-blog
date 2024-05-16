1547d9c1367fac800621e6c7dd95159eff01f3a2
RenanSui <renanddtao@gmail.com>
2024-05-16 14:06:58 -0300

    add avatar image to user avatar

--- src/components/user-avatar.tsx
+++ src/components/user-avatar.tsx
@@ -1,7 +1,7 @@
 import { getUser } from '@/lib/actions/user'
 import { cn } from '@/lib/utils'
 import * as React from 'react'
-import { Avatar, AvatarFallback } from './ui/avatar'
+import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 
 type UserAvatarProps = React.HTMLAttributes<HTMLSpanElement> & {
   user?: Awaited<ReturnType<typeof getUser>>
@@ -12,7 +12,7 @@ export default function UserAvatar({ className, user }: UserAvatarProps) {
 
   return user ? (
     <Avatar className={cn('size-8', className)}>
-      {/* <AvatarImage src={user.imageUrl} alt={user.username ?? ''} /> */}
+      <AvatarImage src={user.imageUrl} alt={user.username} />
       <AvatarFallback className="capitalize">{initials}</AvatarFallback>
     </Avatar>
   ) : null
