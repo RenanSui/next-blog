e12e1a0a2aa5d84bd0e9cccd4d199302b03199a2
RenanSui <renanddtao@gmail.com>
2024-05-17 17:34:19 -0300

    reduce minimal search characters to 1

--- src/lib/validations/search.ts
+++ src/lib/validations/search.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import * as zfd from 'zod-form-data'
 
 export const searchSchema = zfd.formData({
-  searchInput: zfd.text(z.string().min(3).max(50)),
+  searchInput: zfd.text(z.string().min(1).max(50)),
 })
 
 export type SearchSchema = z.infer<typeof searchSchema>
