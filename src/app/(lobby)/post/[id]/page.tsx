238e41107b3a1908b09434113f98e99ce7386416
RenanSui <renanddtao@gmail.com>
2024-05-17 20:58:25 -0300

    update post id page

--- src/app/(lobby)/post/[id]/page.tsx
+++ src/app/(lobby)/post/[id]/page.tsx
@@ -1,62 +1,39 @@
+import { PostCard } from '@/components/post-card'
+import { PostCardSkeleton } from '@/components/post-card-skeleton'
+import { Shell } from '@/components/shell'
 import { getPostById } from '@/lib/actions/post'
+import { ArrowLeftIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
+import React from 'react'
 
 export default async function Page({
   params: { id },
-  searchParams: { index },
 }: {
   params: { id: string }
-  searchParams: { index: string }
 }) {
-  const { error, post } = await getPostById(id)
+  const { post } = await getPostById(id)
 
   return (
-    <div className="max-w-screen-lg mx-auto">
-      {post ? (
-        <section className="space-y-8 py-16">
-          <header>
-            <h1 className="text-3xl font-medium capitalize">{post.title}</h1>
-          </header>
-          <main>
-            <div className="grid grid-cols-4 md:space-x-2 space-y-8 md:space-y-0">
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img
-                src={`https://source.unsplash.com/random/${1280 + Number(index)}x${720 + Number(index)}`}
-                alt="dummy image"
-                className="shadow-xl bg-neutral-950/30 rounded-md col-span-4 md:col-span-2"
-              />
-              <p className="text-lg tracking-wide font-light col-span-4 md:col-span-2">
-                {post.body}
-              </p>
-            </div>
-          </main>
-          <footer>
-            <Link href={`/#post-${index}`} className="hover:underline">
-              Go back
-            </Link>
-          </footer>
-        </section>
-      ) : null}
-
-      {error ? (
-        <section className="space-y-8 py-16">
-          <header>
-            <h1 className="text-3xl font-medium capitalize">
-              An error has ocurred.
-            </h1>
-          </header>
-          <main>
-            <p className="text-lg tracking-wide font-light">
-              {error.message}...
-            </p>
-          </main>
-          <footer>
-            <Link href={`/#post-${index}`} className="hover:underline">
-              Go back
-            </Link>
-          </footer>
-        </section>
-      ) : null}
-    </div>
+    <Shell variant="sidebar" className="pt-0 md:py-0 gap-0">
+      <Link
+        className="py-4 px-4 border-b border-border flex items-center space-x-4"
+        href="/"
+      >
+        <div className="hover:bg-border/60 p-2 rounded-full">
+          <ArrowLeftIcon className="size-5" />
+        </div>
+        <span className="text-xl font-medium">Post</span>
+      </Link>
+      <section>
+        <h1 className="sr-only">Posts</h1>
+        <React.Suspense
+          fallback={Array.from({ length: 10 }).map((_, i) => (
+            <PostCardSkeleton key={i} />
+          ))}
+        >
+          {post ? <PostCard post={post} /> : null}
+        </React.Suspense>
+      </section>
+    </Shell>
   )
 }
