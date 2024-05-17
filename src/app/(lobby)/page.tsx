c6c9c3d92f3d85412d01a917a0b8dfdb9bfe1ad6
RenanSui <renanddtao@gmail.com>
2024-05-17 17:24:44 -0300

    update lobby page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -1,13 +1,15 @@
 import { getPosts } from '@/lib/actions/post'
+import { getMe } from '@/lib/actions/user'
 import * as React from 'react'
 import { Lobby } from './_components/lobby'
 
 export default async function IndexPage() {
   const postsPromise = getPosts()
+  const userPromise = getMe()
 
   return (
     <React.Suspense fallback={<div>Aoba</div>}>
-      <Lobby postsPromise={postsPromise} />
+      <Lobby postsPromise={postsPromise} userPromise={userPromise} />
     </React.Suspense>
   )
 }
