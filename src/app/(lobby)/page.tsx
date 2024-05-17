a05aad8ae5b3465630fe7a765b36fbe3fd9b28bf
RenanSui <renanddtao@gmail.com>
2024-05-17 19:25:40 -0300

    add lobby skeleton to lobby component

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -2,13 +2,14 @@ import { getPosts } from '@/lib/actions/post'
 import { getMe } from '@/lib/actions/user'
 import * as React from 'react'
 import { Lobby } from './_components/lobby'
+import { LobbySkeleton } from './_components/lobby-skeleton'
 
 export default async function IndexPage() {
   const postsPromise = getPosts()
   const userPromise = getMe()
 
   return (
-    <React.Suspense fallback={<div>Aoba</div>}>
+    <React.Suspense fallback={<LobbySkeleton />}>
       <Lobby postsPromise={postsPromise} userPromise={userPromise} />
     </React.Suspense>
   )
