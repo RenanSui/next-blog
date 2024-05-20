cc86878bbc1c21a079bb40c3cee112bde6a7674b
RenanSui <renanddtao@gmail.com>
2024-05-20 16:03:21 -0300

    modify page style

--- src/app/(lobby)/profile/[username]/page.tsx
+++ src/app/(lobby)/profile/[username]/page.tsx
@@ -1,8 +1,9 @@
 import { PostCardSkeleton } from '@/components/post-card-skeleton'
+import { PostFormShell } from '@/components/post-form-shell'
 import { Shell } from '@/components/shell'
 import { UserAvatar } from '@/components/user-avatar'
 import { getPostByUserId } from '@/lib/actions/post'
-import { getUserByUsername } from '@/lib/actions/user'
+import { getMe, getUserByUsername } from '@/lib/actions/user'
 import { ArrowLeftIcon } from '@radix-ui/react-icons'
 import Link from 'next/link'
 import * as React from 'react'
@@ -14,30 +15,34 @@ export default async function Page({
 }: {
   params: { username: string }
 }) {
-  const user = await getUserByUsername(username)
-  if (!user) return null
+  const user = await getMe()
 
-  const { posts } = await getPostByUserId(user._id)
-  const createdAtDate = new Date(user.createdAt)
+  const userByUsername = await getUserByUsername(username)
+  if (!userByUsername) return null
+
+  const { posts } = await getPostByUserId(userByUsername._id)
+  const createdAtDate = new Date(userByUsername.createdAt)
   const monthJoined = createdAtDate.toLocaleString('default', { month: 'long' })
 
   return (
-    <Shell variant="main" className="pt-0 md:py-0 gap-0">
+    <Shell variant="sidebar" className="pt-0 md:py-0 gap-0">
       <Link className="py-2 px-4 flex items-center space-x-4" href="/">
         <div className="hover:bg-border/60 p-2 rounded-full">
           <ArrowLeftIcon className="size-5" />
         </div>
-        <span className="text-xl font-bold">{user.name}</span>
+        <span className="text-xl font-bold">{userByUsername.name}</span>
       </Link>
       <Profile className="space-y-8">
         <ProfileHeader>
           <UserAvatar
             className="rounded-full size-32 bg-black absolute -translate-y-1/2 left-4 border-4 border-white dark:border-black"
-            user={user}
+            user={userByUsername}
           />
           <div className="pt-20 flex flex-col -space-y-1 px-4">
-            <span className="font-bold text-xl">{user.name}</span>
-            <span className="text-muted-foreground/50">@{user.username}</span>
+            <span className="font-bold text-xl">{userByUsername.name}</span>
+            <span className="text-muted-foreground/50">
+              @{userByUsername.username}
+            </span>
             <span className="flex gap-1 pt-3 text-muted-foreground/50">
               Joined {monthJoined} {createdAtDate.getFullYear()}
             </span>
@@ -45,6 +50,9 @@ export default async function Page({
         </ProfileHeader>
         <ProfileContent className="border-t">
           <h1 className="sr-only">Posts</h1>
+          {user?._id === userByUsername._id ? (
+            <PostFormShell user={user} />
+          ) : null}
           <React.Suspense
             fallback={Array.from({ length: 7 }).map((_, i) => (
               <PostCardSkeleton key={i} />
